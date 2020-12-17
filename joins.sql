4ab4fbe3d5768632987b5cc89ecd6e4f775650ec
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-12-16 21:59:17 -0300

    Inserido joins utilizando inner join e outer joins

--- joins.sql
+++ joins.sql
@@ -62,3 +62,41 @@ inner join tb_funcao  using (id_funcao)
 select f.data_inicio, e.data_admissao, f.data_termino
 from tb_empregado e
 inner join tb_historico_funcao f on (e.data_admissao between f.data_inicio and f.data_termino);
+
+-- Using 
+
+-- com uso de using para exibir o id_departamento o mesmo deve estar distinto do apelido da tabela 
+select e.nome, d.nm_departamento , id_departamento 
+from tb_empregado e 
+inner join tb_departamento d using (id_departamento); -- na clausula o id deve estar sozinho 
+
+-- Varios joins com using
+select e.nome, f.base_salario, e.salario, f.teto_salario, d.nm_departamento,f.ds_funcao ,id_funcao
+from tb_empregado e 
+inner join tb_departamento d using (id_departamento)
+inner join tb_funcao f using (id_funcao)
+where to_number(length(e.nome) + length(e.sobrenome)) > 15
+order by 1;
+
+
+--Left Outer join recupera os empregados mesmo eles nao estando vinculado a um departamento 
+select e.nome, id_departamento, d.nm_departamento
+from tb_empregado e 
+left outer join tb_departamento d using (id_departamento)
+where id_departamento is null;
+
+--right outer join recupera os departaentos mesmo n~ao estando vinculados a nenhum empregado. 
+select e.nome, id_departamento, d.nm_departamento 
+from tb_empregado e
+right outer join tb_departamento d using (id_departamento);
+
+-- full outer join recupera todos empregados e departamentos mesmo n~ao estando vinculados um com o outro. 
+select e.nome, id_departamento, d.nm_departamento 
+from tb_empregado e
+full outer join tb_departamento d using (id_departamento);
+
+--CROSS JOIN 
+-- força um produto cartesiano. 
+select * 
+from tb_empregado 
+cross join tb_departamento;
