cce2ccb4aacb5e6446e764b7d3b06b7f30c1fa59
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2021-01-21 17:15:36 -0300

    Finalizado subconsultas INLINE

--- subconsultas.sql
+++ subconsultas.sql
@@ -42,6 +42,7 @@ HAVING avg(salario) < (SELECT MAX(AVG(salario))
 ORDER BY 2 desc; 
 
 --FROM INLINE
+--Nao pode ter order by na subconsulta
 
 SELECT id_departamento 
 FROM (SELECT id_departamento
@@ -53,12 +54,21 @@ SELECT d.nm_departamento,d.id_departamento, empregado.count
 FROM tb_departamento d, (SELECT id_departamento, count(id_departamento) count
                          FROM tb_empregado
                          GROUP BY id_departamento) empregado
-WHERE d.id_departamento = empregado.id_departamento;
+WHERE d.id_departamento = empregado.id_departamento
+ORDER BY 1 desc;
 
 
+--Mesmo resultado sem subconsulta
+SELECT d.nm_departamento, e.id_departamento, count(e.id_departamento)
+FROM tb_departamento d
+INNER JOIN tb_empregado e on(e.id_departamento = d.id_departamento)
+GROUP BY e.id_departamento , d.nm_departamento
+ORDER BY 1 desc;
 
 
-
-select id_funcao, avg(salario), count(id_funcao)
-from tb_empregado
-group by id_funcao;
+--Usando subconsulta no FROM para contar o numero de empregados que possuem cada funcao 
+SELECT  empregado.id_funcao,f.ds_funcao, empregado.CONTADOR
+FROM tb_funcao f , (SELECT id_funcao, count(id_funcao) CONTADOR
+                  FROM tb_empregado
+                  GROUP BY id_funcao) empregado
+WHERE f.id_funcao = empregado.id_funcao;   
